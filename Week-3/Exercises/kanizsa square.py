7dcdd73be869cd63b65e1d4fee272b214577bf9d
Charlie Gilardoni <charlie.gilardoni@gmail.com>
2025-09-25 09:06:20 +0200

    kanizsa square update

--- Week-3/Exercises/kanizsa square.py
+++ Week-3/Exercises/kanizsa square.py
@@ -13,11 +13,24 @@ width, height = exp.screen.size
 radius_circle =width//20
 square_size = width//4
 
-circle = stimuli.Circle(radius=radius_circle)
-colour_circle=[(255,255,255), (0,0,0)]
-x, y = square_size//2
+x = square_size//2
+y = square_size//2
+
+#colour_circle=[(255,255,255), (255,255,255), (0,0,0), (0,0,0)]
 position_circle = [(-x,y), (x,y), (x,-y), (-x,-y)]
 
+circles = []
+for pos in position_circle:
+    circle = stimuli.Circle(radius=radius_circle, colour=(255,255,255), position=(pos))
+    circles.append(circle)
+
+for circle in circles:
+    circle.present(clear=False, update=False)
+
+grey_square = stimuli.Rectangle(size=(square_size,square_size), colour=(0,0,0))
+grey_square.present(clear=False, update=False)
+
+exp.screen.update()
 
 # Leave it on-screen until a key is pressed
 exp.keyboard.wait()
